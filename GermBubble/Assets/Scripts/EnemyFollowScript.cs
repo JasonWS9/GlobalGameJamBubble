630d40eac809a2dca9b41a04d2ea9277a65b2cab
David Aduku <130676739+DavidAAwesome@users.noreply.github.com>
2025-01-25 14:55:53 -0500

    Changed EnemyFollowScript

--- GermBubble/Assets/Scripts/EnemyFollowScript.cs
+++ GermBubble/Assets/Scripts/EnemyFollowScript.cs
@@ -4,17 +4,46 @@ using UnityEngine;
 
 public class EnemyFollowScript : MonoBehaviour
 {
-    public GameObject player;
+    public Transform player;
     public Rigidbody2D rb;
     public float moveSpeed;
-    public float comfyDistance;
 
+
+    public bool shoots;
+    public GameObject bullet;
+    public float comfyDistance;
+    public float coolDown = 3f;
+    public float shotspeed = 6f;
+
+    private Vector2 shootDirection;
+    private float timer = 0.0f;
     Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        movement = player.transform.position - transform.position;
+        movement = player.position - transform.position;
+        // if (shoots)
+        // {
+        //     // Calculate the direction vector from the center to the mouse.
+        //     Vector3 directionToPlayer = player.position - transform.position;
+        //
+        //     // Calculate the rotation while keeping the Y-axis (yaw) rotation fixed.
+        //     Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, directionToPlayer);
+        //     transform.rotation = targetRotation;
+        //     
+        //     shootDirection = player.position - transform.position;
+        //     if (timer >= coolDown)
+        //     {
+        //         //Instantiate a canon ball
+        //         GameObject shot = Instantiate(bullet, transform.position, Quaternion.identity);
+        //         //Apply force
+        //         Rigidbody2D rb = shot.GetComponent<Rigidbody2D>();
+        //         rb.AddForce(shotspeed * shootDirection.normalized, ForceMode2D.Force);
+        //         timer = 0.0f;
+        //     }
+        //     timer += Time.deltaTime;
+        // }
     }
 
     private void FixedUpdate()
